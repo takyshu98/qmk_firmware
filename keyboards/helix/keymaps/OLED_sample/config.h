a62325b8e3f77de0fa549562e15527e56236839f
ISII takesi <isii@harmony.co.jp>
2017-11-08 15:43:14 +0900

    change RGBLED_NUM value

--- keyboards/helix/keymaps/OLED_sample/config.h
+++ keyboards/helix/keymaps/OLED_sample/config.h
@@ -46,7 +46,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 6
+#define RGBLED_NUM 13
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
