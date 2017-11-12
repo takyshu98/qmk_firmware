547809e7f4b13c80fb7a95f60de3db733a659b0c
ISII takesi <isii@harmony.co.jp>
2017-11-13 02:27:45 +0900

    swap Shift & Ctrl like normal keyboard

--- keyboards/helix/keymaps/OLED_sample/keymap.c
+++ keyboards/helix/keymaps/OLED_sample/keymap.c
@@ -88,9 +88,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  \   |
    * |------+------+------+------+------+-------------+------+------+------+------+------|
-   * | Shift|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Shift|
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Ctrl |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * | Ctrl |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Ctrl |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |Adjust|Lower |Raise | Alt  | GUI  |Enter |Space | GUI  | Alt  |Raise |Lower |Lower |
    * `-----------------------------------------------------------------------------------'
@@ -98,8 +98,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = KEYMAP( \
       KC_ESC,  KC_1,  KC_2,   KC_3,   KC_4,   KC_5,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    KC_BSPC, \
       KC_TAB,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS, \
-      KC_LSFT, KC_A,  KC_S,   KC_D,   KC_F,   KC_G,     KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_RSFT, \
-      KC_LCTL, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,     KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RCTL, \
+      KC_LCTL, KC_A,  KC_S,   KC_D,   KC_F,   KC_G,     KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_RCTL, \
+      KC_LSFT, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,     KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
       ADJUST,  LOWER, RAISE, KC_LALT, KC_LGUI, KC_ENT,  KC_SPC, KC_RGUI, KC_RALT,  RAISE, LOWER, LOWER    \
       ),
 
